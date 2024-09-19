5b6190eb7cf49f57c2544c4c8bb53b9e88cddfe4
Gagan-Polukonda <gaganpolukonda@gmail.com>
2024-09-19 01:37:25 -0700

    Basics

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 # initialize the blueprint
 views = Blueprint(__name__, "views")
@@ -6,5 +6,32 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return "home page"
+    return render_template("index.html", name="Gagan")
 
+
+# returning index
+@views.route("/index")
+def index():
+    args = request.args
+    name = args.get('name')
+    return render_template("index.html", name=name)
+
+@views.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+# returning json
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'Gagan', 'coolness': 10})
+
+# get data from a request(in json format)
+# and redirect
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("views.home"))
